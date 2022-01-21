80448642c54b0b2d530176a276bcdfd5ee99865e
bitrequest <mail@bitrequest.io>
2022-01-21 17:37:41 +0100

    detect IPFS

--- assets_js_bitrequest_payments.js
+++ assets_js_bitrequest_payments.js
@@ -1812,7 +1812,8 @@ function share(thisbutton) {
             return false;
         }
         var newdatastring = (thisdata === true) ? "&d=" + dataparam : "", // construct data param if exists
-            sharedurl = "https://bitrequest.github.io/?p=requests&payment=" + payment + "&uoa=" + thiscurrency + "&amount=" + thisamount + "&address=" + thisaddress + newdatastring,
+        	shared_host = (thishostname.indexOf("bafybeihwn7qa3uhdndoa3n7ua7swjyxhqdeztspm342cxl7eaglavxvdna") > -1 || thishostname.indexOf("bitrequest.crypto") > -1) ? thishostname : "https://bitrequest.github.io", // check for IFPS
+            sharedurl = shared_host + "/?p=requests&payment=" + payment + "&uoa=" + thiscurrency + "&amount=" + thisamount + "&address=" + thisaddress + newdatastring,
             thisrequestname_uppercase = thisrequestname.substr(0, 1).toUpperCase() + thisrequestname.substr(1), // capitalize requestname
             paymentupper = payment.substr(0, 1).toUpperCase() + payment.substr(1),
             sharedtitle = (thisdata === true) ? thisrequestname_uppercase + " sent a " + paymentupper + " payment request of " + thisamount + " " + thiscurrency.toUpperCase() + " for '" + thisrequesttitle + "'" : "You have a " + paymentupper + " payment request of " + thisamount + " " + thiscurrency,
