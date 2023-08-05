04174d936e3da86b08a8455b1fb4ed65a58077f6
bitrequest <mail@bitrequest.io>
2023-08-05 21:17:00 +0200

    proxy path

--- proxy/v1/ln/tor/index.php
+++ proxy/v1/ln/tor/index.php
@@ -13,7 +13,7 @@ function fetch_tor($url, $data, $headers) {
     }
     // Call default proxy if TOR is not installed
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://www.bitrequest.app/tor/proxy/v1/ln/tor/api/");
+    curl_setopt($ch, CURLOPT_URL, "https://www.bitrequest.app/proxy/v1/ln/tor/api/");
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($plo));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
