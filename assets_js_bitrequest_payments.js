8ea2c872b22d23f2312e551841f69346fa20dc70
bitrequest <mail@bitrequest.io>
2023-11-30 21:59:58 +0100

    clear beautifier line

--- assets_js_bitrequest_payments.js
+++ assets_js_bitrequest_payments.js
@@ -3019,10 +3019,4 @@ function xmr_integrated(xmr_address, pmid) {
     }
     console.log("invalid xmr payment id");
     return xmr_address;
-} // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
 }
