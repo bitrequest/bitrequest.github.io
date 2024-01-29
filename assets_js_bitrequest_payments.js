65a8fa44e98c407f12a39cd7d666ea76fa4c5d46
bitrequest <mail@bitrequest.io>
2024-01-29 11:47:25 +0100

    remove keycodes

--- assets_js_bitrequest_payments.js
+++ assets_js_bitrequest_payments.js
@@ -1983,7 +1983,6 @@ function validatesteps() {
         let thisnode = $(this),
             thisvalue = thisnode.val(),
             keycode = e.keyCode;
-        alert(keycode);
         if (keycode === 188 || keycode === 190 || keycode === 108 || keycode === 110 || keycode === 229) { // prevent double commas and dots
             if (thisvalue.indexOf(".") > 0 || thisvalue.indexOf(",") > 0) {
                 e.preventDefault();
