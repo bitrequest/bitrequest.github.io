6e83ad16c967032ca9ea4c5b3bce86826485d10e
bitrequest <mail@bitrequest.io>
2024-12-16 14:57:42 +0100

    remove jquery dependency

--- assets_js_lib_bitrequest_checkout.js
+++ assets_js_lib_bitrequest_checkout.js
@@ -1,6 +1,6 @@
-const html_node = $("html");
+const html_node = document.documentElement;
 
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
     checkout();
     window.addEventListener("message", crossframe);
     keyup();
@@ -9,13 +9,17 @@ $(document).ready(function() {
 
 // Handles the checkout process when a checkout button is clicked
 function checkout() {
-    $(document).on("click", ".br_checkout", function(e) {
+    document.addEventListener("click", function(e) {
+        const target = e.target;
+        if (!target.matches(".br_checkout")) return;
+        
         e.preventDefault();
-        const checkout_url = $(this).attr("href"),
-            br_frame = $("#br_framebox iframe");
-        if (br_frame.length > 0) {
+        const checkout_url = target.getAttribute("href"),
+            br_frame = document.querySelector("#br_framebox iframe");
+        
+        if (br_frame) {
             showloader();
-            br_frame.attr("src", checkout_url);
+            br_frame.setAttribute("src", checkout_url);
         } else {
             append_iframe(checkout_url);
             showloader();
@@ -25,15 +29,17 @@ function checkout() {
 
 // Appends an iframe to the body with the given source URL
 function append_iframe(framesrc) {
-    $("body").append("<div id='br_framebox'><iframe src='" + framesrc + "'></iframe></div><div id='br_loadbox'><div id='br_loadpanel'><div id='br_loader'></div><p>Loading request...</p></div></div>");
+    const div = document.createElement("div");
+    div.innerHTML = "<div id='br_framebox'><iframe src='" + framesrc + "'></iframe></div><div id='br_loadbox'><div id='br_loadpanel'><div id='br_loader'></div><p>Loading request...</p></div></div>";
+    document.body.appendChild(div.firstElementChild);
     iframe_loaded();
 }
 
 // Sets up a load event listener for the iframe
 function iframe_loaded() {
-    const requestframe = $("#br_framebox iframe");
-    requestframe.on("load", function() {
-        if (requestframe.attr("src")) {
+    const requestframe = document.querySelector("#br_framebox iframe");
+    requestframe.addEventListener("load", function() {
+        if (requestframe.getAttribute("src")) {
             showframe();
         }
     });
@@ -68,7 +74,7 @@ function result_callback(post_data) {
 
 // Shows the iframe by adding CSS classes
 function showframe() {
-    html_node.addClass("showframe zoomframe");
+    html_node.classList.add("showframe", "zoomframe");
 }
 
 // Prompts for confirmation before closing the frame
@@ -80,44 +86,51 @@ function closeframe_confirm() {
 
 // Closes the iframe by removing CSS classes
 function closeframe() {
-    if (html_node.hasClass("zoomframe")) {
-        html_node.removeClass("zoomframe");
+    if (html_node.classList.contains("zoomframe")) {
+        html_node.classList.remove("zoomframe");
         setTimeout(function() {
-            html_node.removeClass("showframe");
-            $("#br_framebox iframe").removeAttr("src");
+            html_node.classList.remove("showframe");
+            const iframe = document.querySelector("#br_framebox iframe");
+            if (iframe) {
+                iframe.removeAttribute("src");
+            }
         }, 400);
     }
 }
 
 // Shows the loader by adding CSS classes
 function showloader() {
-    html_node.addClass("slide_loader fade_loader");
+    html_node.classList.add("slide_loader", "fade_loader");
 }
 
 // Sets up a click event listener to close the loader
 function closeloader_trigger() {
-    $(document).on("click", "#br_loadbox", closeloader);
+    document.addEventListener("click", function(e) {
+        if (e.target.matches("#br_loadbox")) {
+            closeloader();
+        }
+    });
 }
 
 // Closes the loader by removing CSS classes
 function closeloader() {
-    if (html_node.hasClass("fade_loader")) {
-        html_node.removeClass("fade_loader");
+    if (html_node.classList.contains("fade_loader")) {
+        html_node.classList.remove("fade_loader");
         setTimeout(function() {
-            html_node.removeClass("slide_loader");
+            html_node.classList.remove("slide_loader");
         }, 1000);
     }
 }
 
 // Sets up a keyup event listener for the ESC key
 function keyup() {
-    $(document).keyup(function(e) {
-        if (e.keyCode === 27) {
-            if (html_node.hasClass("slide_loader")) {
+    document.addEventListener("keyup", function(e) {
+        if (e.key === "Escape" || e.keyCode === 27) {
+            if (html_node.classList.contains("slide_loader")) {
                 closeloader();
                 return false;
             }
-            if (html_node.hasClass("showframe")) {
+            if (html_node.classList.contains("showframe")) {
                 closeframe_confirm();
                 return false;
             }
